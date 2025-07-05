490bde8bcb9c33ab4267153e877c888e8e0fe913
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-05 01:38:21 +0000

    Install Nodemon to watch changes during development

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import Car from "./src/car"
 
 let toyotaCorolla = new Car("Toyota", "Corolla")
 toyotaCorolla.startEngine()
+
+let fordMustang = new Car("Ford", "Mustang")
+fordMustang.startEngine()
